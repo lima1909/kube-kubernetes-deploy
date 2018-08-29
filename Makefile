0a5a5e8744c7080a4a4e5faba34f0bc27f8fb5a1
Mario Linke <lima1909@github.com>
2018-08-29 22:35:28 +0200

    add openshift/api

--- Makefile
+++ Makefile
@@ -7,3 +7,9 @@ test:
 deps:
 	govendor add +external github.com/openshift/origin/^
 	govendor add +external github.com/openshift/client-go/^
+
+
+# more documentation
+pre-dep:
+	go get -u -d github.com/openshift/api
+	git checkout tags/v3.9.0
--- main.go
+++ main.go
@@ -1,12 +1,14 @@
 package main
 
 import (
+	"github.com/openshift/api/route/v1"
 	"k8s.io/kubernetes/pkg/kubectl/cmd"
 	"k8s.io/kubernetes/pkg/kubectl/cmd/util"
 	"k8s.io/kubernetes/pkg/kubectl/genericclioptions"
 )
 
 func main() {
+	_ = v1.SchemeGroupVersion
 
 	cf := genericclioptions.NewConfigFlags()
 	factory := util.NewFactory(cf)
